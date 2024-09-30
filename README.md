828fe33c62f6d99d88c96188ee55972a05c710e8
jrodriguezarias <jrodriguezarias@itba.edu.ar>
2024-09-30 15:58:36 -0300

    Terminamos README

--- README.md
+++ README.md
@@ -19,14 +19,14 @@ Esto fue implementado mediante llamadas recursivas donde se utilizó una copia d
 
 ## Parte 3: Poda del árbol
 
-Debido al alto numero de posiciones del tablero futuras que puede haber, en especial al comienzo de la partida, el algoritmo debe evaluar una cantidad muy alta de situaciones. Para el tablero utilizado de 64 posiciones, puede haber un maximo de 60 movimientos entre ambos jugadores. Como cada uno de estas posiciones debe evaluar un movimiento (implicando que llama a la funcion playMove y getValidMoves), la complejidad computacional aumenta de forma factorial. 
+Debido al alto numero de posiciones del tablero futuras que puede haber, en especial al comienzo de la partida, el algoritmo debe evaluar una cantidad muy alta de situaciones. Para el tablero utilizado de 64 posiciones, puede haber un maximo de 33 movimientos validos cada turno. Como cada uno de estos movimientos implica una nueva posicion (o nodo) para evaluar, la complejidad computacional aumenta de forma exponencial. Por lo tanto, se puede tomar O(33**n) como cota superior del calculo computacional.
 
 Como consecuencia, es funcionalmente imposible evaluar el arbol entero y es necesario limitar o bien la cantidad de posiciones (o nodos) que pueden evaluarse, o bien la profundidad (es decir, la cantidad de movimientos hacia adelante) que el algoritmo explora. Debido a que utilizamos busqueda DFS, utilizamos ambos tipos de limitaciones para podar el arbol asi no se consumen todos los nodos sin poder haber recorrido el arbol de forma pareja (es decir, que no se utilicen todos los nodos permitidos en los hijos de un mismo nodo padre sino que se evaluen tambien los hijos de los nodos hermanos al nodo padre en cuestion).
-
  
 ## Documentación adicional
 
-[Aquí.]
+Demostracion de la cota maxima de movimientos validos de un turno dado de Reversi/Othello
+https://jxiv.jst.go.jp/index.php/jxiv/preprint/view/480/1498
 
 ## Bonus points
 
--- ai.cpp
+++ ai.cpp
@@ -41,59 +41,69 @@ Square getBestMove(GameModel &model)
 
 
 int miniMax(GameModel model, int depth, int alpha, int beta, bool isMax) {
+    // Node count. Increased every time the function is called to keep count of nodes evaluated
     static int nodes;
     nodes++;
+
 
     Moves validMoves;
     getValidMoves(model, validMoves);
     
     Player currentPlayer = (getCurrentPlayer(model) == PLAYER_WHITE)? PLAYER_WHITE : PLAYER_BLACK;
     int index; // index of the best move according to the algorithm
-    GameModel tempModel = model;
+    GameModel tempModel = model; // model used for looking into future moves without disrupting the ongoing game
 
-    if(depth == 0 || model.gameOver || nodes == MAX_NODES){
+    if(depth == 0 || model.gameOver || nodes == MAX_NODES){ // Heuristic evaluation 
         return getScore(model, currentPlayer);
     }
 
 
     int i;
-    if(isMax) {
+    if(isMax) {// maximizing player
         long int max = -INF;
 
 
-        for(i = 0; i < validMoves.size() && nodes < MAX_NODES; i++) {
+        for(i = 0; i < (validMoves.size() - 1) && nodes < MAX_NODES; i++) {
             tempModel = model;
             playMove(tempModel, validMoves[i]);
 
-            int value = miniMax(model, depth - 1, alpha, beta, !isMax);
+            int value = miniMax(model, depth - 1, alpha, beta, !isMax);//recursive call
             max = ((max <  value) ? value : max);
+
+            /******************************
+             * Alpha pruning module *
+             ******************************/
             alpha = (alpha < max) ? max : alpha;
             if(beta <= alpha) {
-                std::cout << "alphaprrr" << std::endl;
+                std::cout << "alpha prune" << std::endl;
                 break; // alpha pruning
             }
 
 
         }
 
-        if(depth == MAX_DEPTH) { // inmmediate next move
-            nodes = 0;
-            return i - 1;
+        if(depth == MAX_DEPTH) { // FINAL RETURN. Returns inmmediate next move's index
+            nodes = 0; // reset nodes to evaluate for the next turn
+            return i;
         }
         return max;
 
     }
-    else {
+    else { // minmizing player
         long int min = INF;
-        for(int i = 0; i < validMoves.size() && nodes < MAX_NODES; i++) {
+        for(int i = 0; i < (validMoves.size() - 1) && nodes < MAX_NODES; i++) {
             tempModel = model;
             playMove(tempModel, validMoves[i]);
 
-            int value = miniMax(tempModel, depth - 1, alpha, beta, !isMax);
+            int value = miniMax(tempModel, depth - 1, alpha, beta, !isMax);//recursive call
             min = ((min >  value) ? value : min);
+
+             /******************************
+             * Beta pruning module *
+             ******************************/
             beta = (beta > min) ? min : beta;
             if(alpha >= beta) {
-                std::cout << "betaprrr" << std::endl;
+                std::cout << "beta prune" << std::endl;
                 break; // beta pruning
             }
 
